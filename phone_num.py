567317815b51d05aead48372986a069b13db2338
lamyaalzahrani2003 <llaammiiaassss2003@gmail.com>
2026-04-22 00:10:51 +0300

    Add files via upload
    "Phone Number Tracking with Python"

---
+++ phone_num.py
@@ -0,0 +1,18 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
+
+print("x" * 50)
+
+entered_num =input ("Please enter a phone number : ")
+
+phone_num = phonenumbers.parse(entered_num, None)  
+print(phone_num)
+
+print(geocoder.description_for_number(phone_num, "en"))
+
+print(carrier.name_for_number(phone_num, "en"))
+
+print(timezone.time_zones_for_number(phone_num),)
+
+
+#phonenumbers.parse test the number if it's real phone number or not
